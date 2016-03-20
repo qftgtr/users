11f6c949683407c73f8cbc62bb88de3033b7ecd6
qftgtr <qft.gtr@gmail.com>
2016-03-20 13:15:56 +0800

    handle 404

--- server/components/errors/index.js
+++ server/components/errors/index.js
@@ -11,12 +11,12 @@ module.exports[404] = function pageNotFound(req, res) {
     status: statusCode
   };
 
-  res.status(result.status);
-  res.render(viewFilePath, {}, function(err, html) {
-    if (err) {
-      return res.status(result.status).json(result);
-    }
-
-    res.send(html);
-  });
+  res.status(result.status).send();
+  // res.render(viewFilePath, {}, function(err, html) {
+  //   if (err) {
+  //     return res.status(result.status).json(result);
+  //   }
+  //
+  //   res.send(html);
+  // });
 };
